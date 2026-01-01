4d93c420831b6fd6d5b4a4a4f80caa4ce26bde7e
Kristen Garnier <kristen@oktopod.io>
2026-01-01 20:01:16 +0100

    Fix : Two hours difference was too short for updates

--- infrastructure/react/src/utils/date.utils.ts
+++ infrastructure/react/src/utils/date.utils.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 export function isRecent(date: Date) {
 	const current = dayjs();
 	const target = dayjs(date);
-	const hoursDiff = Math.abs(current.diff(target, "hour"));
+	const daysDiff = Math.abs(current.diff(target, "day"));
 
-	return hoursDiff <= 2;
+	return daysDiff <= 1;
 }
